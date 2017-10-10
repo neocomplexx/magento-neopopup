feef6cf843b2db1e2cf4f8e45b10e488b9f88a40
Mauro Gesuitti <mauro@neocomplexx.com>
2017-10-10 10:47:31 -0300

    5 segundo para mostrar

--- js/neocomplexx/neopopup/neopopup.js
+++ js/neocomplexx/neopopup/neopopup.js
@@ -1,6 +1,6 @@
 (function($) {
     $(document).ready(function(){
-        var delayMillis = 1000; //1 second
+        var delayMillis = 5000; //1 second
         setTimeout(function() {
             console.log("Showing newsletter modal");
             $("#neocomplexx_modal_newsletter").modal();
